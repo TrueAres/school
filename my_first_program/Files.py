4fde925c71578ab5003010e495df79d65395d367
TrueAres <3000richardm@gmail.com>
2026-06-17 16:08:47 -0700

    Delete my_first_program directory

--- my_first_program/Files.py
+++
@@ -1,7 +0,0 @@
-num = 0
-try:
-    num = int(float(input("Enter a number: ")))
-except:
-    print("why are you like this")
-
-print(num)
--- my_first_program/zybooks.py
+++
@@ -1,6 +0,0 @@
-pigments_file_name = input()
-pigment_data = input()
-
-with open(pigments_file_name, "w") as pigments_file:
-    pigments_file.write(pigment_data)
-    pigments_file.write("\n")
