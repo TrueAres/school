7f0ae7c84b707f7f50c18caf2d8457e640b21fa5
TrueAres <3000richardm@gmail.com>
2026-06-24 13:46:05 -0700

    Add files via upload

--- project/my_first_program/zybooks.py
+++ project/my_first_program/zybooks.py
@@ -1,22 +1,23 @@
-class Rectangle:
-    def __init__(self):
-        self.length = 0
-        self.width = 0
+class Passenger:
+    def __init__(self, num_checked_bags, travel_time, seat): 
+        self.num_checked_bags = num_checked_bags
+        self.travel_time = travel_time
+        self.seat = seat
 
-    """ Your code goes here """
-    def print_attributes(self):
-        return "Length: " + self.length + ", width: " + self.width
+    def lost_checked_bag(self):
+        self.num_checked_bags -= 1
+
+    def print_data(self):
+        print(f"Passenger data: {self.num_checked_bags} checked bags, {self.travel_time} hours travel time, seat {self.seat}")
+
+num_checked_bags1 = int(input())
+travel_time1 = int(input())
+seat1 = input()
+
+""" Your code goes here """
+passenger1 = Passenger(num_checked_bags1, travel_time1, seat1)
 
 
-
-
-rectangle1 = Rectangle()
-rectangle1.length = int(input())
-rectangle1.width = int(input())
-
-rectangle2 = Rectangle()
-rectangle2.length = int(input())
-rectangle2.width = int(input())
-
-rectangle1.print_attributes(input())
-rectangle2.print_attributes(input())
+print(f"Passenfer data: {passenger1.num_checked_bags} checked bags, {passenger1.travel_time} hours travel time, seat {passenger1.seat}")
+passenger1.lost_checked_bag()
+print(f"Passenfer data: {passenger1.num_checked_bags} checked bags, {passenger1.travel_time} hours travel time, seat {passenger1.seat}")
